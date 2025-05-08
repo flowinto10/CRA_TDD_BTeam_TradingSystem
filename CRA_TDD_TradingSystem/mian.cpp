cf4a56c1da937357d920835ae1939e088c05d5ce
DESKTOP-5FBE16T\User <sokaman@naver.com>
2025-05-08 15:18:24 +0900

    no message

--- CRA_TDD_TradingSystem/mian.cpp
+++ CRA_TDD_TradingSystem/mian.cpp
@@ -11,15 +11,19 @@ class MockDriver;
 // StockBroker Driver 생성
 TEST(StockTS, CreateStockDriver) {
 	StockBroker* app = new TestStockBroker{};
+	
+	
+
 
 	EXPECT_NE(nullptr, app);
 }
 
 // StockBroker Driver 생성
 TEST(StockTS, CreateMockkDriver) {
-	MockDriver* app = new MockDriver{};
-
-	EXPECT_NE(nullptr, app);
+	MockDriver mock;
+	EXPECT_CALL(mock, login("haha", "haha"))
+		.Times(1);
+	mock.login("haha", "haha");
 }
 
 
