02047b38876676da355b26abcdbcfaa151e5a819
youngjo park <anyways5188@gmail.com>
2025-05-08 16:11:30 +0900

    buy/sell tc

--- CRA_TDD_TradingSystem/mian.cpp
+++ CRA_TDD_TradingSystem/mian.cpp
@@ -3,11 +3,27 @@
 #include "mockdriver.cpp"
 #include "ats.cpp"
 
+#include <string>
+
+using namespace std;
 using namespace testing;
 
-class ATS; // Auto Trading System
-class StockBroker;
-class MockDriver;
+class StockItem : public Test {
+protected:
+	void SetUp() override {
+		stockTr = { "0070", 10000, 100 };
+	}
+private:
+	struct stockTransaction {
+		string STOCK_CODE;
+		int STOCK_PRICE;
+		int STOCK_COUNT;
+	} stockTr;
+public:
+	StockBroker* ats = new TestStockBroker{};
+	MockDriver* mock = new MockDriver{};
+};
+
 
 // StockBroker Driver 생성
 TEST(StockTS, CreateStockDriver) {
@@ -23,7 +39,6 @@ TEST(StockTS, CreateMockkDriver) {
 	EXPECT_NE(nullptr, app);
 }
 
-
 // ATS 생성
 TEST(StockTS, CreateATS) {
 	//ATS app;
@@ -32,6 +47,25 @@ TEST(StockTS, CreateATS) {
 	EXPECT_NE(nullptr, app);
 }
 
+TEST(StockTS, BuyStock0) {
+	MockDriver mock;
+
+	EXPECT_CALL(mock, buy(_, _, _))
+		.Times(1);
+
+	ATS* app = new ATS{&mock};
+	EXPECT_EQ(true, app->buy("0080", 10000, 10));
+}
+
+TEST(StockTS, SellStock0) {
+	MockDriver mock;
+
+	EXPECT_CALL(mock, sell(_, _, _))
+		.Times(1);
+
+	ATS* app = new ATS{&mock};
+	EXPECT_EQ(true, app->sell("0080", 10000, 10));
+}
 
 int main() {
 	::testing::InitGoogleMock();
