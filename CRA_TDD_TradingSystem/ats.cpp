f802f285f71949f055de0118d974ea8900fc91ff
youngjo park <anyways5188@gmail.com>
2025-05-08 15:44:38 +0900

    buy/sell tc
    (cherry picked from commit dce95402c6ea7a37271cafbcc05752d58a3fe5fb)

--- CRA_TDD_TradingSystem/ats.cpp
+++ CRA_TDD_TradingSystem/ats.cpp
@@ -1,8 +1,24 @@
+#include "stock_broker.h"
+#include <string>
+
+using namespace std;
+
 class ATS {
 public:
 	ATS(){}
+	ATS(StockBroker* stockBroker) : stockBroker{stockBroker}
+	{}
 
+	void selectStockBroker();
+	bool buy(string code, int price, int count) { 
+		stockBroker->buy(code, price, count);
+		return true; };
+	bool sell(string code, int price, int count) { 
+		stockBroker->sell(code, price, count);
+		return true; };
+	int getPrice(string code, int price, int count) {};
 
 private:
+	StockBroker* stockBroker;
 
 };
